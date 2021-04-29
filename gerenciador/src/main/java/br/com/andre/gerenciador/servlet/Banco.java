e5f3f4ec4a7ea36992e911cdf4761a7cc1a363ad
Andre0Luis <65985134+Andre0Luis@users.noreply.github.com>
2021-04-28 22:28:00 -0300

    [ALT] Lista de empresas

--- gerenciador/src/main/java/br/com/andre/gerenciador/servlet/Banco.java
+++ gerenciador/src/main/java/br/com/andre/gerenciador/servlet/Banco.java
@@ -8,6 +8,25 @@ import java.util.List;
 public class Banco {
 	
 	private static List<Empresa> lista = new ArrayList<>();
+	
+	//bloco estatico para não cadastrar tudo novamente.
+	static {
+		Empresa empresa = new Empresa();
+		empresa.setNome("FNC");
+		empresa.setCnpj("22.564.123/0001-52");
+		
+		Empresa empresa2 = new Empresa();
+		empresa2.setNome("ALURA");
+		empresa2.setCnpj("22.125.456/0001-65");
+		
+		Empresa empresa3 = new Empresa();
+		empresa3.setNome("J.A BAZAR E PAPELARIA");
+		empresa3.setCnpj("22.829.062/0001-43");
+		
+		lista.add(empresa);
+		lista.add(empresa2);
+		lista.add(empresa3);
+	}
 
 	public void adiciona(Empresa empresa) {
 		lista.add(empresa);
